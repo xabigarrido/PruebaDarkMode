0333d2cdce8517d09cabd846a5835fa4abe00027
xabi <xabigarrido@gmail.com>
2022-10-03 07:27:23 +0200

    Primera subida

--- App.js
+++ App.js
@@ -1,11 +1,17 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-
+import { StyleSheet, Switch, Text, View } from 'react-native';
+import {useColorScheme} from 'nativewind'
 export default function App() {
+  const {colorScheme, toggleColorScheme} = useColorScheme()
+  console.log(colorScheme)
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    // <View style={styles.container}>
+    //   <Text>Open up App.js to start working on your app!</Text>
+    //   <StatusBar style="auto" />
+    // </View>
+    <View className="flex-1 justify-center items-center bg-slate-300 dark:bg-black">
+      <Switch value={colorScheme == "dark"} onChange={toggleColorScheme}/>
+      <Text className="dark:text-white">Hola mundo</Text>
     </View>
   );
 }
